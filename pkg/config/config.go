83b9820a6945502fcbab4c199364716874c16d17
Kirill Murzin <murz.kirill@yandex.ru>
2023-02-24 13:39:53 +0500

    helpers

---
+++ pkg/config/config.go
@@ -0,0 +1,26 @@
+package config
+
+import (
+	"github.com/spf13/viper"
+	"log"
+)
+
+var config *viper.Viper
+
+// Init is an exported method that takes the environment starts the viper
+// (external lib) and returns the configuration struct.
+func Init(env string) {
+	var err error
+	config = viper.New()
+	config.SetConfigType("json")
+	config.SetConfigName(env)
+	config.AddConfigPath("configs/")
+	err = config.ReadInConfig()
+	if err != nil {
+		log.Fatal("error on parsing configuration file", err)
+	}
+}
+
+func GetConfig() *viper.Viper {
+	return config
+}
